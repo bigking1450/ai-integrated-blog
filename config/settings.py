41a8a0ffab9ee6d55e25e6fcf4d1144dcaf0558b
Kamba <umarkamba@yahoo.com>
2026-09-14 12:38:26 +0100

    collected static files and updated settings,py file for deployment

--- config/settings.py
+++ config/settings.py
@@ -170,3 +170,18 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+
+    SECURE_PROXY_SSL_HEADER = (
+        "HTTP_X_FORWARDED_PROTO",
+        "https",
+    )
+
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = False
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
